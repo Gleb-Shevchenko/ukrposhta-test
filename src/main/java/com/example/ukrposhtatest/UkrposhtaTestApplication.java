e021278763526d0480afb6e8eaca5d8e8eb47580
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2023-05-05 11:00:00 +0300

    first commit

---
+++ src/main/java/com/example/ukrposhtatest/UkrposhtaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ukrposhtatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkrposhtaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkrposhtaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukrposhtatest/UkrposhtaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukrposhtatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkrposhtaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
